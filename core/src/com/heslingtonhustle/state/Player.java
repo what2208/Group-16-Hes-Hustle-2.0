9c2b6d60c08323585e2a5acf1671ebba517a53af
Sam Hartley <121134897+samh366@users.noreply.github.com>
2024-04-28 20:31:41 +0100

    Added a way to track the player's distance travelled

--- core/src/com/heslingtonhustle/state/Player.java
+++ core/src/com/heslingtonhustle/state/Player.java
@@ -9,7 +9,9 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 
-/** Represents the player character. Is responsible for the movement of the player. */
+/**
+ * Represents the player character. Is responsible for the movement of the player.
+ * */
 public class Player {
     public static final float SPEED = 0.1f;
 
@@ -20,6 +22,7 @@ public class Player {
     private final float width;
     private final float height;
     private final float scale = 2f;
+    private float distanceTravelled = 0;
 
     /**
      * @param startingX Spawn location
@@ -51,18 +54,22 @@ public class Player {
                 case MOVE_LEFT:
                     facing = Facing.LEFT;
                     setPosition(position.x-speed, position.y);
+                    distanceTravelled += speed;
                     break;
                 case MOVE_RIGHT:
                     facing = Facing.RIGHT;
                     setPosition(position.x+speed, position.y);
+                    distanceTravelled += speed;
                     break;
                 case MOVE_UP:
                     facing = Facing.UP;
                     setPosition(position.x, position.y+speed);
+                    distanceTravelled += speed;
                     break;
                 case MOVE_DOWN:
                     facing = Facing.DOWN;
                     setPosition(position.x, position.y-speed);
+                    distanceTravelled += speed;
                     break;
                 default:
                     break;
@@ -204,5 +211,19 @@ public class Player {
      */
     public HashSet<Action> getMovement() {
         return movement;
+    }
+
+    /**
+     * @return The number of units the player has moved
+     */
+    public float getDistanceTravelled() {
+        return distanceTravelled;
+    }
+
+    /**
+     * Sets the player's distance travelled to 0
+     */
+    public void resetDistanceTravelled() {
+        distanceTravelled = 0;
     }
 }
