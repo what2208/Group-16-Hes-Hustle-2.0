24e31de078ad614aa45b093595fe9b2534581233
Sam Hartley <121134897+samh366@users.noreply.github.com>
2024-04-30 00:34:03 +0100

    Update Activity tests to reflect changes
    Activity class was reworked to more clearly represent an activity
    Added a tests to test creation and completion of an activity

--- tests/src/io/skloch/heshustle/tests/ActivityTests.java
+++ tests/src/io/skloch/heshustle/tests/ActivityTests.java
@@ -6,18 +6,60 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ActivityTests {
+    /**
+     * Tests whether an Activity correctly returns information about
+     * itself
+     */
     @Test
-    public void testIncreaseValue() {
-        Activity activity = new Activity();
-        int increaseValue = 1;
-        int newCounter = 1;
-        int newTimesPerformedToday = 1;
+    public void getActivityAttributes() {
+        Activity activity = new Activity(
+                "fishing",
+                "recreational",
+                100,
+                15,
+                2,
+                3
+        );
 
-        activity.increaseValue(increaseValue);
 
-        assertEquals(increaseValue, activity.getValue());
-        assertEquals(newCounter, activity.getCount());
-        assertEquals(newTimesPerformedToday, activity.getTimesPerformedToday());
+        // Test attributes
+        assertEquals("fishing", activity.getName());
+        assertEquals("recreational", activity.getScoreType());
+        assertEquals(100, activity.getScore());
+        assertEquals(15, activity.getEnergyUse());
+        assertEquals(2, activity.getHours());
+
+        // Test other baseline attributes
+        assertEquals(0, activity.getTimesCompleted());
+        assertEquals(0, activity.getTimesCompletedToday());
+        assertEquals(0, activity.getHoursSpent());
+
+    }
+
+
+    /**
+     * A test to ensure an Activity's attributes update correctly when
+     * the activity is completed.
+     */
+    @Test
+    public void testCompleteActivity() {
+        Activity activity = new Activity(
+                "fishing",
+                "recreational",
+                100,
+                15,
+                2,
+                3
+        );
+
+
+        // Should increase the number of times it has been completed,
+        // and the hours spent doing it
+        activity.completeActivity();
+
+        assertEquals(1, activity.getTimesCompleted());
+        assertEquals(1, activity.getTimesCompletedToday());
+        assertEquals(2, activity.getHoursSpent());
     }
 
 //counter++;
