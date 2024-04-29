0e25ad5d56306c7166aebbe8672d261e5d2222a3
Sam Hartley <121134897+samh366@users.noreply.github.com>
2024-04-29 01:42:42 +0100

    Added achievements and penalties to game over screen
    Added achievements to the game over screen along with a title and description for each.
    Negative penalties are also displayed if the user did not complete enough of any of the tasks

--- core/src/com/heslingtonhustle/screens/GameOverScreen.java
+++ core/src/com/heslingtonhustle/screens/GameOverScreen.java
@@ -11,16 +11,17 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.heslingtonhustle.HeslingtonHustleGame;
 import com.heslingtonhustle.sound.Sounds;
+import com.heslingtonhustle.state.Achievement;
 import com.heslingtonhustle.state.Activity;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * A screen to display the user's score, along with any secret achievements
  * or debuffs they encountered.
  */
 public class GameOverScreen implements Screen {
-    private final HeslingtonHustleGame heslingtonHustleGame;
     private final Stage stage;
     private Table optionsTable;
     private final Texture backgroundTexture;
@@ -29,12 +30,17 @@ public class GameOverScreen implements Screen {
     private int[] stats;
 
 
+    private HashSet<Achievement> achievements = new HashSet<>();
+    private int numAchievements = 0;
+    private final HeslingtonHustleGame parentClass;
+
+
     /**
      * Creates the screen and fills in all the user's score information
      * @param parentClass
      */
     public GameOverScreen(HeslingtonHustleGame parentClass, HashMap<String, Activity> activities, int[] stats) {
-        heslingtonHustleGame = parentClass;
+        this.parentClass = parentClass;
         this.activities = activities;
         this.stats = stats;
 
@@ -47,12 +53,28 @@ public class GameOverScreen implements Screen {
         Image backgroundImage = new Image(backgroundTexture);
         stage.addActor(backgroundImage);
 
-        setupGUI(parentClass);
+        // Figure out which achievements/penalties the player has got
+        getAchievements();
+        getPenalties();
+
+        // Show the player's score breakdown
+        drawScorePaper();
+        // Draw banners for penalties and achievements
+        drawBanners();
 
     }
 
+    /**
+     * Calculates the final score the player got
+     * @return The player's score
+     * TODO: Change
+     */
+    private int calcScore() {
+        return 1400;
+    }
 
-    private void setupGUI(HeslingtonHustleGame parentClass) {
+
+    private void drawScorePaper() {
 
         // Page texture
         pageTexture = new Texture("Graphics/UI/Gameover/page.png");
@@ -73,8 +95,8 @@ public class GameOverScreen implements Screen {
                 activities.get("study").getCount(),
                 activities.get("recreation").getCount(),
                 activities.get("eat").getCount(),
-                stats[1],
-                stats[2]
+                numAchievements,
+                achievements.size() - numAchievements
         };
 
         // Lines
@@ -98,7 +120,7 @@ public class GameOverScreen implements Screen {
         }
 
         // Score text
-        Label scoreText = new Label("Score: ", parentClass.skin, "handwriting64px");
+        Label scoreText = new Label("Score: " + calcScore(), parentClass.skin, "handwriting64px");
         scoreText.setPosition((parentClass.WIDTH - gameOver.getWidth()) / 2 - 20, 93);
         stage.addActor(scoreText);
 
@@ -112,13 +134,52 @@ public class GameOverScreen implements Screen {
             @Override
             public void changed(ChangeListener.ChangeEvent event, Actor actor) {
                 parentClass.soundController.playSound(Sounds.CONFIRM);
-                heslingtonHustleGame.changeScreen(AvailableScreens.MenuScreen);
+                parentClass.changeScreen(AvailableScreens.MenuScreen);
             }
         });
+    }
 
+    /**
+     * Draws banners and explanations for the secret achievements the player
+     * reached
+     */
+    private void drawBanners() {
+        Table achievementTable = new Table();
+        Table penaltyTable = new Table();
+
+        for (Achievement achievement : achievements) {
+            TextButton banner = new TextButton(achievement.getTitle(), parentClass.skin, "banner");
+            Label description = new Label(achievement.getDescription(), parentClass.skin, "achievementlabel");
+
+            if (achievement.isPositive()) {
+                // Achievement
+                achievementTable.add(banner).padBottom(20).prefWidth(300);
+                achievementTable.row();
+                achievementTable.add(description).padBottom(20);
+                achievementTable.row();
+            } else {
+                // Penalty
+                banner.setDisabled(true);
+                penaltyTable.add(banner).padBottom(20).prefWidth(300);
+                penaltyTable.row();
+                penaltyTable.add(description).padBottom(20);
+                penaltyTable.row();
+            }
+
+        }
+
+        // Add both tables to the stage
+        achievementTable.setWidth(300);
+        achievementTable.setPosition(parentClass.WIDTH-350, (parentClass.HEIGHT - achievementTable.getHeight()) / 2 + 30);
+        stage.addActor(achievementTable);
+
+        penaltyTable.setWidth(300);
+        penaltyTable.setPosition(60, (parentClass.HEIGHT - achievementTable.getHeight()) / 2 + 30);
+        stage.addActor(penaltyTable);
 
 
     }
+
 
     @Override
     public void show() { }
@@ -151,4 +212,68 @@ public class GameOverScreen implements Screen {
         stage.dispose();
         backgroundTexture.dispose();
     }
+
+
+    /**
+     * Determines the secret achievements the player has achieved
+     * based on the activities completed.
+     * Adds these to the set of achievements
+     * TODO: Make these conditional
+     */
+    private void getAchievements() {
+        achievements.add(new Achievement(
+                "Walker",
+                "Walk 200 steps each day",
+                200
+        ));
+
+        achievements.add(new Achievement(
+                "Clubber",
+                "Go clubbing at least once",
+                500
+        ));
+
+        achievements.add(new Achievement(
+                "Duck duck go",
+                "Feed the ducks 6 times",
+                300
+        ));
+
+        numAchievements += 3;
+
+    }
+
+    /**
+     * Adds negative achievements, or 'penalties' if the player has not eaten
+     * or studied enough etc.
+     * TODO: Make these conditional
+     */
+    private void getPenalties() {
+        achievements.add(new Achievement(
+                "Zombie",
+                "You didn't get enough sleep!",
+                -500
+        ));
+
+        achievements.add(new Achievement(
+                "Clueless",
+                "You didn't study enough!",
+                -1000
+        ));
+
+        achievements.add(new Achievement(
+                "Hungry",
+                "You didn't eat well enough!",
+                -500
+        ));
+
+        achievements.add(new Achievement(
+                "Party Pooper",
+                "You didn't have enough fun!",
+                -200
+        ));
+    }
+
+
+
 }
