aae19eeeeb8195976a29972c1909a43b4e4c878f
Sam Hartley <121134897+samh366@users.noreply.github.com>
2024-05-12 00:43:39 +0100

    Reformatted the position and look of the map
    Removed yellow patches of ground and used a different grass tileset

--- assets/Maps/Tilesets/test.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="test" tilewidth="16" tileheight="16" tilecount="48" columns="12">
- <image source="../../../../../Downloads/Serene_Village_revamped_v1.9/SERENE_VILLAGE_REVAMPED/Construct 3/Autotiles_no_inner_corners_16x16.png" width="192" height="64"/>
-</tileset>
