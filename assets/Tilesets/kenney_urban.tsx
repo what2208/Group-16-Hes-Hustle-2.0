f229db23101e60fae98b50296099222ac3423b10
what2208 <og731@york.ac.uk>
2024-05-13 21:12:27 +0100

    Added triggers for switching between campus and town
    Also added some town NPCs

--- assets/Tilesets/kenney_urban.tsx
+++ assets/Tilesets/kenney_urban.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="urban" tilewidth="16" tileheight="16" spacing="1" tilecount="486" columns="27">
- <image source="../../Users/owend/Downloads/kenney_rpgUrbanKit/Tilemap/tilemap.png" width="458" height="305"/>
+ <image source="assets/Graphics/tilemap.png" width="458" height="305"/>
  <wangsets>
   <wangset name="Pavement" type="corner" tile="-1">
    <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
